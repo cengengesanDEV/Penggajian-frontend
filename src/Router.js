469cd250c651a134b6e2e11d06b5f1aac09e05df
farisan <muhammad.farisan99@gmail.com>
2023-04-14 22:46:06 +0700

    setup

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+// import PrivateElement from "./component/PrivateElement";
+// import PrivateElementAuth from "./component/PrivateElementAuth";
+
+import Home from "./pages/Home";
+
+
+const router = createBrowserRouter([
+  // { path: "/", element: <App />, errorElement: <Error /> },
+  { path: "/", element: <Home /> },
+
+
+]);
+
+export default router;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import css from '../style/Home.module.css'
+
+function Home() {
+  return (
+    <>
+    <p className={css.tester}>asdasd</p>
+    </>
+  )
+}
+
+export default Home
