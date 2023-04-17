f9d9ba53dc3af09c4b3ebd6db28b9abe18f499a6
farisan <muhammad.farisan99@gmail.com>
2023-04-17 19:06:13 +0700

    add router home

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 // import PrivateElement from "./component/PrivateElement";
 // import PrivateElementAuth from "./component/PrivateElementAuth";
 
-import Home from "./pages/Home";
+import Home from "./pages/Tester";
 
 
 const router = createBrowserRouter([
