11ef8b3bfac269f73e0495b989e2b445f0b040bc
farisan <muhammad.farisan99@gmail.com>
2023-04-17 19:06:22 +0700

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,72 @@
-import React from 'react'
-
-import css from '../style/Home.module.css'
-
-function Home() {
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+const Home = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <>
-    <p className={css.tester}>asdasd</p>
-    </>
-  )
-}
-
-export default Home
+    <Layout>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            float: 'left',
+            width: 120,
+            height: 31,
+            margin: '16px 24px 16px 0',
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: console.log(key),
+            label: `nav ${index + 1}`,
+          }))}
+          // onClick={console.log("sss")}
+        />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+export default Home;
