48890c722cc4f25323e585d618faa7664581b9cf
imbasri <imbasri.dev@gmail.com>
2023-05-01 12:58:49 +0700

    update data

--- src/pages/hr/Listkaryawan.js
+++ src/pages/hr/Listkaryawan.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Navbar from "../../components/Navbar";
 
 function ListKaryawan() {
   return (
-    <>
-    <Navbar />
     <div>ListKaryawan</div>
-    </>
   )
 }
 
